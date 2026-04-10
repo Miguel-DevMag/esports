b521ce81c1e3424d039d5c0af0c625d550aafc63
Miguel-DevMag <mangellaura006@gmail.com>
2026-04-10 10:43:25 -0300

    Configurando o sistema para user JavaScript (Vite

---
+++ resources/views/exemplo.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo JS com Vite</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <p id="texto">Texto original</p>
+    <button id="btn">Clique aqui</button>
+</body>
+</html>
