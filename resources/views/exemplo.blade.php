f99d016222440a7730c1cc8a4abf06009deb0227
Miguel-DevMag <mangellaura006@gmail.com>
2026-04-17 11:28:25 -0300

    inserindo um video como background no meu projeto esports, e atualizando os  links no header

--- resources/views/exemplo.blade.php
+++ resources/views/exemplo.blade.php
@@ -2,23 +2,33 @@
 <html>
 <head>
     <title>Exemplo JS com Vite</title>
+
     @vite('resources/css/style.css')
     @vite('resources/js/app.js')
-</head>
+
+</head> 
 <body>
     <div id="home" class="container">
    <div class="hero">
+
     <header class="navbar">
         <img src="#" alt="logo">
         <div>
                 <a href="#home">Home</a>
                 <a href="#esportes">Esportes</a>
                 <a href="#sobre">Sobre</a>
-                <a href="#jogadores">Jogadores</a>
-            
+                <a href="#jogadores">Jogadores</a>   
         </div> 
+
     </header>
+
+    <video autoplay loop muted playsinline class="back-video">
+    <source src="{{ asset('storage/videos/hero-futebol.mp4') }}" type="video/mp4">
+    </video>
+    <div class="overlay"></div>
+
    </div>
+
    <main>
     <section id="esportes" class="cards-sports">
         <div>
@@ -49,6 +59,6 @@
    <footer>
        <p>&copy; 2026 Esportes. Todos os direitos reservados.</p>
    </footer>
-   </div>
+     </div>
 </body>
 </html>
